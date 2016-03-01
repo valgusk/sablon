20b83d29f262dc698aacd678e01f6f3a94a22e69
Valery Guskov <valerijs.gusjkovs@tieto.com>
2016-03-01 16:02:43 +0200

    added image support

--- lib/sablon/parser/mail_merge.rb
+++ lib/sablon/parser/mail_merge.rb
@@ -1,6 +1,12 @@
 module Sablon
   module Parser
     class MailMerge
+      attr_accessor :resources
+
+      def initialize(resources)
+        self.resources = resources
+      end
+
       class MergeField
         KEY_PATTERN = /^\s*MERGEFIELD\s+([^ ]+)\s+\\\*\s+MERGEFORMAT\s*$/
 
@@ -21,7 +27,230 @@ module Sablon
         end
 
         def get_display_node(node)
-          node.search(".//w:t").first
+          node.search(".//w:t").first || node.search(".//w:instrText").first
+        end
+      end
+
+      # class
+
+      class IncludeImage < MergeField
+
+        class SizeCalculator < Struct.new(:node, :resources, :x, :y, :image_x, :image_y, :offx, :offy, :realx, :realy)
+          require 'yaml'
+          def initialize(node, realx, realy, resources)
+            self.realx = realx
+            self.realy = realy
+            self.node = node
+            self.x = extent_node['cx'].to_i
+            self.y = extent_node['cy'].to_i
+            self.image_x = shape_extent_node['cx'].to_i
+            self.image_y = shape_extent_node['cy'].to_i
+            self.offx = src_rect_node['r'].to_i
+            self.offy = src_rect_node['b'].to_i
+            self.resources = resources
+
+
+            configuration['width'] ||= 'rect'
+            configuration['height'] ||= 'rect'
+          end
+
+          def update_id
+            newid = node.document.root.search('.//w:drawing//wp:docPr').map{ |pr| pr['id'].to_i }.max + resources.size
+            property_node['id'] = newid
+          end
+
+          def apply
+            extent_node['cx'] = newx
+            extent_node['cy'] = newy
+
+            shape_extent_node['cx'] = newx
+            shape_extent_node['cy'] = newy
+
+            stretch = stretch_node
+            stretch.children.map(&:remove)
+
+            update_id
+
+            src_rect_node['l'] = format_percent(new_off_x[0].to_f / new_image_x.to_f) if new_off_x[0] != 0
+            src_rect_node['r'] = format_percent(new_off_x[1].to_f / new_image_x.to_f) if new_off_x[1] != 0
+            src_rect_node['t'] = format_percent(new_off_y[0].to_f / new_image_y.to_f) if new_off_y[0] != 0
+            src_rect_node['b'] = format_percent(new_off_y[1].to_f / new_image_y.to_f) if new_off_y[1] != 0
+
+            property_node['descr'] =  property_node['descr'].sub(META_PATTERN, '')
+          end
+
+          def format_percent(number)
+            parts = (number.to_f * 100).divmod(1)
+            "#{ parts[0] }#{ (1000 * parts[1]).round.to_s.rjust(3, '0') }"
+          end
+
+          def configuration
+            @config ||= (property_node['descr'].to_s =~ META_PATTERN) ? parse_config($1) : {}
+          end
+
+          def full_auto?
+            ![configuration['height'], configuration['width']].include?('rect')
+          end
+
+          def newy
+            configuration['height'] == 'auto' ? auto_y : y
+          end
+
+          def newx
+            configuration['width'] == 'auto' ? auto_x : x
+          end
+
+          def auto_x
+            if full_auto?
+              if (realy.to_f / y.to_f ) <= (realx.to_f / x.to_f)
+                x
+              else
+                ((auto_y * realx) / realy).to_i
+              end
+            else
+              ((y * realx) / realy).to_i
+            end
+          end
+
+          def auto_y
+            if full_auto?
+              if (realy.to_f / y.to_f ) > (realx.to_f / x.to_f)
+                y
+              else
+                ((auto_x * realy) / realx).to_i
+              end
+            else
+              ((x * realy) / realx).to_i
+            end
+          end
+
+          def new_image_x
+            configuration['width'] == 'rect' ? x : auto_x
+          end
+
+          def new_image_y
+            configuration['height'] == 'rect' ? y : auto_y
+          end
+
+          def new_off_x
+            {
+              'rect' => [0, 0],
+              'auto' => [0, 0],
+              'left' => [0, 0],
+              'right' => [new_image_x - newx, 0],
+              'middle' => [((new_image_x - newx) / 2).to_i]*2
+            }[configuration['width']]
+          end
+
+          def new_off_y
+            {
+              'rect' => [0,0],
+              'auto' => [0,0],
+              'top' => [0,0],
+              'bottom' => [new_image_y - newy, 0],
+              'middle' => [((new_image_y - newy) / 2).to_i]*2,
+            }[configuration['height']]
+          end
+
+          private
+
+          def src_rect_node
+            ret = node.at_xpath('.//a:srcRect', 'a' => MAIN_NS_URI)
+            return ret if ret
+
+            stretch_node.before '<a:srcRect/>'
+            src_rect_node
+          end
+
+          def property_node
+            node.search('.//wp:docPr').first
+          end
+
+          def extent_node
+            node.search('.//wp:extent').first
+          end
+
+          def shape_extent_node
+            node.
+              at_xpath('.//pic:spPr', 'pic' => PICTURE_NS_URI).
+              at_xpath('.//a:ext', 'a' => MAIN_NS_URI)
+          end
+
+          def stretch_node
+            node.
+              at_xpath('.//pic:blipFill', 'pic' => PICTURE_NS_URI).
+              at_xpath('.//a:stretch', 'a' => MAIN_NS_URI)
+          end
+
+          def parse_config(hash)
+            indent = -1
+            YAML.load(
+              hash.gsub(/([\{\},]\s*)|(=>)/) do |match|
+                case match[0]
+                when '{'
+                  indent += 1
+                  "\n#{ '  ' * indent }"
+                when '}'
+                  indent = [0, indent - 1].max
+                  "\n#{ '  ' * indent }"
+                when ','
+                  "\n#{ '  ' * indent }"
+                when '='
+                  ': '
+                end
+              end
+            )
+          rescue Psych::SyntaxError
+            {}
+          end
+        end
+
+        PICTURE_NS_URI = 'http://schemas.openxmlformats.org/drawingml/2006/picture'
+        MAIN_NS_URI = 'http://schemas.openxmlformats.org/drawingml/2006/main'
+        KEY_PATTERN = /^(=[^ ]+)/
+        META_PATTERN = /^\!(\{[^\}]+\})/
+
+        attr_accessor :resources
+
+        def initialize(node, resources)
+          self.resources = resources
+          @node = node
+          @raw_expression = node.at_xpath('.//pic:cNvPr', 'pic' => PICTURE_NS_URI)['name'].strip
+        end
+
+        def replace(content)
+          if content.is_a?(Content::Image) && node = @node
+            image_id = next_image_id
+            replace_image(node, image_id)
+            calc = SizeCalculator.new(node, content.spec.width, content.spec.height, resources)
+            calc.apply
+            resources[image_id] = content
+          end
+        end
+
+        def replace_image(node, image_id)
+          node.at_xpath('.//a:blip', 'a' => MAIN_NS_URI)['r:embed'] = image_id
+        end
+
+        def next_image_id
+          rid = resources.keys.map{ |rid| (rid[/^rId\d+$/] && rid.sub('rId', '')).to_i }.max.to_i + 1
+          "rId#{ rid }"
+        end
+
+        def remove
+          false
+        end
+
+        def expression
+          $1 if @raw_expression =~ KEY_PATTERN
+        end
+
+        class << self
+          def valid_candidate?(node)
+            return false if node.name != 'drawing'
+            prop = node.at_xpath('.//pic:cNvPr', 'pic' => PICTURE_NS_URI)
+            prop['name'].strip[/^=/]
+          end
         end
       end
 
@@ -96,6 +325,8 @@ module Sablon
         xml.traverse do |node|
           if node.name == "fldSimple"
             field = SimpleField.new(node)
+          elsif node.name == 'drawing'
+            field = IncludeImage.new(node, resources) if IncludeImage.valid_candidate?(node)
           elsif node.name == "fldChar" && node["w:fldCharType"] == "begin"
             field = build_complex_field(node)
           end
--- lib/sablon/template.rb
+++ lib/sablon/template.rb
@@ -20,20 +20,67 @@ module Sablon
     def render(context, properties = {})
       Sablon::Numbering.instance.reset!
       Zip.sort_entries = true # required to process document.xml before numbering.xml
+
+      # parse resources
+      resources = {}
+
+      resources_xml = Zip::File.open(@path).get_entry('word/_rels/document.xml.rels').get_input_stream.read
+      resources_document = Nokogiri::XML.parse(resources_xml)
+      resource_collection = resources_document.search('Relationships').first
+      relationships = resource_collection.search('Relationship')
+      relationships.each { |r| resources[r['Id'].to_s] = r }
+      # parse resources end
+
       Zip::OutputStream.write_buffer(StringIO.new) do |out|
-        Zip::File.open(@path).each do |entry|
+        opened_zip = Zip::File.open(@path)
+        contents = {}
+
+        opened_zip.each do |entry|
           entry_name = entry.name
-          out.put_next_entry(entry_name)
+
           content = entry.get_input_stream.read
+
           if entry_name == 'word/document.xml'
-            out.write(process(Processor::Document, content, context, properties))
+            contents[entry_name] = process(Processor::Document, content, context, resources, properties)
           elsif entry_name =~ /word\/header\d*\.xml/ || entry_name =~ /word\/footer\d*\.xml/
-            out.write(process(Processor::Document, content, context))
+            contents[entry_name] = process(Processor::Document, content, context, resources)
           elsif entry_name == 'word/numbering.xml'
-            out.write(process(Processor::Numbering, content))
+            contents[entry_name] = process(Processor::Numbering, content)
+            # out.write(Processor.process_rels(Nokogiri::XML(content), resources).to_xml)
+          elsif entry_name == 'word/_rels/document.xml.rels'
+            false
+          elsif entry_name == '[Content_Types].xml'
+            types = Nokogiri::XML.parse(content)
+            ['jpeg', 'png', 'bmp'].each do |type|
+              parent = types.children.first
+              unless parent.children.select{ |t| t['ContentType'] == "image/#{ type }" }.any?
+                parent.add_child(%{<Default Extension="#{ type }" ContentType="image/#{ type }"/>})
+              end
+            end
+
+            contents[entry_name] = types.to_xml(indent: 0).gsub("\n","")
           else
-            out.write(content)
+            contents[entry_name] =content
           end
+        end
+
+        resources.each do |id, r|
+          next if r.is_a?(Nokogiri::XML::Node)
+
+          name = "auto#{ id.downcase }.#{ r.spec.content_type[/png|jpeg|bmp/] }"
+          xml = %{<Relationship Id="#{ id }" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target="media/#{ name }"/>}
+          node = Nokogiri::XML::parse(xml).children.first
+          resource_collection.add_child(node)
+
+          contents[File.join('word', 'media', name)] = File.read(r.data)
+          # resources[id] = node
+        end
+
+        contents[File.join('word', '_rels', 'document.xml.rels')] = resources_document.to_xml(indent: 0).gsub("\n","")
+
+        contents.keys.sort.each do |entry_name|
+          out.put_next_entry(entry_name)
+          out.write(contents[entry_name])
         end
       end
     end
